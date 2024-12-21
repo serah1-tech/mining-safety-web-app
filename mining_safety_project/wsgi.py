283a0a7b224ac0ff42dace25e682f12e4866039c
Ndogo1 <wangariserah99@gmail.com>
2024-12-21 20:06:52 +0300

    modification of wsgi.py to waitress

--- mining_safety_project/wsgi.py
+++ mining_safety_project/wsgi.py
@@ -1,25 +1,8 @@
-"""
-WSGI config for mining_safety_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
-# mining_safety_project/wsgi.py
-
 import os
 from django.core.wsgi import get_wsgi_application
-<<<<<<< HEAD
-=======
-from waitress import serve
->>>>>>> eaf0b04b0ec7948f2daf38a93a6c21582c7377b2
 
+# Set the default Django settings module for the 'application' callable.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mining_safety_project.settings')
 
+# Get the WSGI application.
 application = get_wsgi_application()
-
-if __name__ == "__main__":
-    serve(application, host='0.0.0.0', port=10000)
-
