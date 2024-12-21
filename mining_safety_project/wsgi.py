eaf0b04b0ec7948f2daf38a93a6c21582c7377b2
Ndogo1 <wangariserah99@gmail.com>
2024-12-21 09:26:50 +0300

    changes from gunicorn to waitress made

--- mining_safety_project/wsgi.py
+++ mining_safety_project/wsgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-
+from waitress import serve
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mining_safety_project.settings')
 
 application = get_wsgi_application()
+
+if __name__ == "__main__":
+    serve(application, host='0.0.0.0', port=10000)
+
